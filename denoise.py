81f5cc7005a43f23e91e15333eeac64ff7d00dbe
sahabi <sahabi@gmail.com>
2016-11-09 11:09:00 -0600

    modified code

--- denoise.py
+++ denoise.py
@@ -3,7 +3,7 @@ import maxflow
 import sys
 from Moutils.Moutils import list_2D
 
-def denoise(evals,param_arg,betaswtr_arg,states):
+def denoise(evals,param_arg,betaswtr_arg):
 
 	param = param_arg
 	betaswtr = betaswtr_arg
@@ -14,6 +14,7 @@ def denoise(evals,param_arg,betaswtr_arg,states):
 	wh = -log(0.5)
 
 	evals.sort()
+	#print evals
 
 	evaluations = evals
 
@@ -22,11 +23,15 @@ def denoise(evals,param_arg,betaswtr_arg,states):
 	g = maxflow.Graph[float](200, 200)
 
 	imageBuilder = list_2D(NEVALS, ROW)
-	imageBuilder_ts = list_2D(NEVALS, ROW)
+	imageBuilder_ts = list_2D(NEVALS, ROW+1)
+	imageBuilder_times = list_2D(NEVALS, ROW)
 
 	for i in range(ROW):
 		for j in range(NEVALS):
 			imageBuilder[i][j] = -1
+			
+	for i in range(ROW+1):
+		for j in range(NEVALS):
 			imageBuilder_ts[i][j] = -1
 
 	for i, row in enumerate(evals):
@@ -34,11 +39,15 @@ def denoise(evals,param_arg,betaswtr_arg,states):
 		action = row[1]
 		evaluation = row[2]
 		ts = row[3]
+		time_step = row[4]
 		if i == 0:
 			currentState = state
 			currentCol = 0
 			imageBuilder[1-action][currentCol] = evaluation
-			imageBuilder_ts[1-action][currentCol] = [evaluation, ts]
+			imageBuilder_ts[1-action][currentCol] = evaluation
+			imageBuilder_times[1-action][currentCol] = [evaluation, time_step]
+			imageBuilder_ts[2][currentCol] = ts
+			#print imageBuilder_ts
 			actionLast = action
 		else:
 			if state == currentState:
@@ -47,20 +56,28 @@ def denoise(evals,param_arg,betaswtr_arg,states):
 					evaluation = 1 - evaluation
 				if imageBuilder[1-action][currentCol] == -1:
 					imageBuilder[1-action][currentCol] = evaluation
-					imageBuilder_ts[1-action][currentCol] = [evaluation, ts]
+					imageBuilder_ts[1-action][currentCol] = evaluation
+					imageBuilder_times[1-action][currentCol] = [evaluation, time_step]
+					imageBuilder_ts[2][currentCol] = ts
 				else:
 					currentCol = currentCol + 1
 					imageBuilder[1-action][currentCol] = evaluation
 					imageBuilder[action][currentCol] = -1
-					imageBuilder_ts[1-action][currentCol] = [evaluation, ts]
-					imageBuilder_ts[action][currentCol] = [-1, ts]
+					imageBuilder_ts[1-action][currentCol] = evaluation
+					imageBuilder_ts[action][currentCol] = -1
+					imageBuilder_times[1-action][currentCol] = [evaluation, time_step]
+					imageBuilder_times[action][currentCol] = [-1, time_step]
+					imageBuilder_ts[2][currentCol] = ts
 			else:
 				currentState = state
 				currentCol = currentCol + 1
 				imageBuilder[1 - action][currentCol] = evaluation
 				imageBuilder[action][currentCol] = -1
-				imageBuilder_ts[1 - action][currentCol] = [evaluation, ts]
-				imageBuilder_ts[action][currentCol] = [-1, ts]
+				imageBuilder_ts[1 - action][currentCol] = evaluation
+				imageBuilder_ts[action][currentCol] = -1
+				imageBuilder_times[1 - action][currentCol] = [evaluation, time_step]
+				imageBuilder_times[action][currentCol] = [-1, time_step]
+				imageBuilder_ts[2][currentCol] = ts
 			actionLast = action
 
 	for i in range(len(evals)):
@@ -76,13 +93,30 @@ def denoise(evals,param_arg,betaswtr_arg,states):
 	imageBuilder_ts = imageBuilder_ts[:][0:nNew+1]
 	imageBuilder = imageBuilder[:][0:nNew+1]	
 
+	# for i in range(ROW):
+	# 	for j in range(nNew):
+	# 		g.add_nodes(1)
+	# 		if imageBuilder[i][j] == -1:
+	# 			if imageBuilder[1-i][j] == 0:
+	# 				g.add_tedge(j+i*nNew,w1,w2)
+	# 			else:
+	# 				g.add_tedge(j+i*nNew,w2,w1)
+	# 		else:
+	# 			if imageBuilder[i][j] == 0:
+	# 				g.add_tedge(j+i*nNew,w1,w2)
+	# 			else:
+	# 				g.add_tedge(j+i*nNew,w2,w1)
+
 	for i in range(ROW):
 		for j in range(nNew):
 			g.add_nodes(1)
 			if imageBuilder[i][j] == 0:
 				g.add_tedge(j+i*nNew,w1,w2)
-			else:
+			elif imageBuilder[i][j] == 1:
 				g.add_tedge(j+i*nNew,w2,w1)
+			else:
+				g.add_tedge(j+i*nNew,wh,wh)
+
 
 	for i in range(nNew - 1):
 		g.add_edge( i,i+1, betaswtr, betaswtr );
@@ -105,7 +139,7 @@ def denoise(evals,param_arg,betaswtr_arg,states):
 		finalDeNoiseImage[0][j] = 1 - deNoiseImage[0][j]
 		finalDeNoiseImage[1][j] = deNoiseImage[1][j]
 
-	return (flow, deNoiseImage, finalDeNoiseImage, imageBuilder, imageBuilder_ts)
+	return (flow, deNoiseImage, finalDeNoiseImage, imageBuilder_ts, imageBuilder, imageBuilder_times)
 
 if __name__ == "__main__":
 	denoise(getEvals(),.3,.5)
